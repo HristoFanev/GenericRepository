aaff164ad935fdc7e30a643246a46c4e93e97102
HristoFanev <107874286+HristoFanev@users.noreply.github.com>
2022-11-25 09:30:50 +0200

    Adding classes in model

---
+++ GenericRepositoryExercise/DataAccess/Model/Breed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Model
+{
+    internal class Breed
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public List<Animal> Animals { get; set; }
+    }
+}
